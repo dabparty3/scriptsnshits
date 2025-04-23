1575635a8b340fb95e731bbaae642492f21494fc
dabparty3 <arealnamereallyreally@gmail.com>
2025-04-23 03:14:10 -0500

    Update uhhhbeatupnoobs.lua

--- uhhhbeatupnoobs.lua
+++ uhhhbeatupnoobs.lua
@@ -6,35 +6,31 @@ _G.DumDumFarm = false
 
 local Window = Rayfield:CreateWindow({
    Name = "somethin i made for convience",
-   Icon = 0, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
+   Icon = 0,
    LoadingTitle = "loadinggggg",
    LoadingSubtitle = "by whaaat8804",
-   Theme = "Serenity", -- Check https://docs.sirius.menu/rayfield/configuration/themes
-
+   Theme = "Serenity",
    DisableRayfieldPrompts = false,
-   DisableBuildWarnings = false, -- Prevents Rayfield from warning when the script has a version mismatch with the interface
-
+   DisableBuildWarnings = false,
    ConfigurationSaving = {
       Enabled = true,
-      FolderName = "pointlessconfig", -- Create a custom folder for your hub/game
+      FolderName = "pointlessconfig",
       FileName = "heythere"
    },
-
    Discord = {
-      Enabled = false, -- Prompt the user to join your Discord server if their executor supports it
-      Invite = "noo", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ ABCD would be ABCD
-      RememberJoins = true -- Set this to false to make them join the discord every time they load it up
+      Enabled = false,
+      Invite = "noo",
+      RememberJoins = true
    },
-
-   KeySystem = true, -- Set this to true to use our key system
+   KeySystem = true,
    KeySettings = {
       Title = "hey there",
       Subtitle = "this key shouldn't be hard",
-      Note = "the key is 'cool'", -- Use this to tell the user how to get a key
-      FileName = "keynamethatkeykeykeyijustwannafindthekey", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
-      SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
-      GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-      Key = {"cool"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+      Note = "the key is 'cool'",
+      FileName = "keynamethatkeykeykeyijustwannafindthekey",
+      SaveKey = true,
+      GrabKeyFromSite = false,
+      Key = {"cool"}
    }
 })
 
